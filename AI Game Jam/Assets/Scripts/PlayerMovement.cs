828c462c2a33428d4ef6e3df3a7c4a437ccd2e2f
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-02-29 11:58:59 +1300

    feat: working on invisible walls to collide with player

--- AI Game Jam/Assets/Scripts/PlayerMovement.cs
+++ AI Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -5,41 +5,62 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private float playerSpeed = 0.5f;
+    [SerializeField] private float playerSpeed = 5f;
     private Vector3 playerPos;
+    private bool colliding = false;
     // Start is called before the first frame update
     void Start()
     {
         playerPos = playerPrefab.transform.position;
     }
 
-    // Update is called once per frame
+//  Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if(!colliding)
         {
-            Debug.Log("D key was pressed");
-            playerPos.x -= playerSpeed;
-            playerPrefab.transform.position = playerPos;
+           
+            if (Input.GetKey(KeyCode.D))
+            {
+                Debug.Log("D key was pressed");
+                playerPos.x -= playerSpeed;
+                playerPrefab.transform.position = playerPos;
+            }
+            else if(Input.GetKey(KeyCode.A))
+            {
+                Debug.Log("A key was pressed");
+                playerPos.x += playerSpeed;
+                playerPrefab.transform.position = playerPos;
+            }
+            else if(Input.GetKey(KeyCode.W))
+            {
+                Debug.Log("W key was pressed");
+                playerPos.z -= playerSpeed;
+                playerPrefab.transform.position = playerPos;
+            }
+            else if(Input.GetKey(KeyCode.S))
+            {
+                Debug.Log("S key was pressed");
+                playerPos.z += playerSpeed;
+                playerPrefab.transform.position = playerPos;
+            }
         }
-        else if(Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("A key was pressed");
-            playerPos.x += playerSpeed;
-            playerPrefab.transform.position = playerPos;
-        }
-        else if(Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("W key was pressed");
-            playerPos.z -= playerSpeed;
-            playerPrefab.transform.position = playerPos;
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            Debug.Log("S key was pressed");
-            playerPos.z += playerSpeed;
-            playerPrefab.transform.position = playerPos;
-        }
+    }
 
+     private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Wall")
+        {
+            colliding = true;
+            print("Colliding with wall");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Wall")
+        {
+            colliding = false;
+        }
     }
 }
