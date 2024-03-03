521dcdafa5572e05c2258898da3a0a440923fded
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-03-04 10:03:15 +1300

    Merged both scenes into Level 1

--- AI Game Jam/Assets/Scripts/PlayerMovement.cs
+++ AI Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -21,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
             playerVelocity.y = 0f; //Ensures that the player doesnt move when it is touching the ground
         }
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //Sets move to the input of each axis (WASD or arrow keys)
-        controller.Move(-move * Time.deltaTime * playerSpeed); //moves the player based on the input of the player, move is multiplied by -1 to make the player move in the correct direction
+        controller.Move(move * Time.deltaTime * playerSpeed); //moves the player based on the input of the player, move is multiplied by -1 to make the player move in the correct direction
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
