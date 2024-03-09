02de1e0d07c2dfa29df41818cf3cbe97c3a3d72d
Erika Stuart <erikat.stuart@gmail.com>
2024-03-09 20:50:44 +1300

    Removed debug logs and commented code

--- AI Game Jam/Assets/Scripts/PickupItem.cs
+++ AI Game Jam/Assets/Scripts/PickupItem.cs
@@ -19,9 +19,7 @@ public class PickupItem : MonoBehaviour
         {
             inRange = true; //set inRange to true
             item = other.gameObject; //assign the item as the item for pickup
-            Debug.Log("In Range");
         }
-        Debug.Log("Entered Trigger");
     }
 
     void OnTriggerExit(Collider collider)
@@ -34,13 +32,11 @@ public class PickupItem : MonoBehaviour
 
     private void PickUp()
     {
-        //happens only onece/when key press
+        //happens only once/when key press
         if (inRange && Input.GetKeyDown(KeyCode.Mouse0)) //if in range and clicks mouse button
         {
-            Debug.Log("Clicked");
             item.transform.parent = gameObject.transform ; //stick to players position
             item.transform.position = gameObject.transform.position + transform.forward * 2; //move the item to the players forward position
-            //item.transform.position = new Vector3(item.transform.position.x, item.transform.position.y + 0.5f, item.transform.position.z); //raise it so it looks like it is off the ground
         }
     }
 }
