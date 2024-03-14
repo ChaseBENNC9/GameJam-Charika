8d18eb5ca14555078542274d45a4d0034ad4c8ea
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-03-15 09:36:07 +1300

    doc: added header comment to item script

--- AI Game Jam/Assets/Scripts/Item.cs
+++ AI Game Jam/Assets/Scripts/Item.cs
@@ -1,3 +1,10 @@
+/*
+* Description: Manages the Item and sets the scale of the item when it is held and when it is placed
+* Author: Chase Bennett-Hill
+* Last Modified: 15 / 03 / 24
+* Last Modified By: Chase Bennett-Hill
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +14,11 @@ public class Item : MonoBehaviour
 
     [SerializeField] private string type;
     private Vector3 placedScale;
-    public Vector3 PlacedScale { get { return placedScale; } }
-    public Vector3 heldScale;
+    public Vector3 PlacedScale { get => placedScale; }
+
+    public Vector3 HeldScale { get => heldScale; }
+
+    private Vector3 heldScale;
 
 
     void Start()
