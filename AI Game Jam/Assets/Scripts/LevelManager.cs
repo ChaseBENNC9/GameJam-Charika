f10175a8d352a999bf441564c7b6cd611af6c260
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-03-26 13:57:48 +1300

    added some comments to explain code and made playerY a const as per requested changes

--- AI Game Jam/Assets/Scripts/LevelManager.cs
+++ AI Game Jam/Assets/Scripts/LevelManager.cs
@@ -2,7 +2,7 @@
 * Description: This is used to manage details of the level such as checkpoints and respawning the player at the active checkpoint.
 * Author: Chase Bennett-Hill
 * Last Modified: 20 / 03 / 2024
-* 
+*
 */
 
 using System.Collections;
@@ -12,10 +12,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private List<Checkpoint> checkpoints;
+    private List<Checkpoint> checkpoints;
     private Checkpoint activeCheckpoint;
-  [SerializeField] private GameObject playerprefab;
-    public static LevelManager instance;
+
+    [SerializeField]
+    private GameObject playerprefab; //the player prefab used to respawn the player
+    public static LevelManager instance; //creates an instance of the level manager so that it can be accessed from other scripts
+    private const float PLAYERY = 1.08f; // the y value of the player object
 
     // Start is called before the first frame update
 
@@ -25,17 +28,17 @@ public class LevelManager : MonoBehaviour
     {
         instance = this;
         checkpoints = new List<Checkpoint>();
-        foreach (Checkpoint checkpoint in FindObjectsOfType<Checkpoint>())
+        foreach (Checkpoint checkpoint in FindObjectsOfType<Checkpoint>()) //iterates through a list of all the checkpoints in the scene
         {
-            checkpoints.Add(checkpoint);
+            checkpoints.Add(checkpoint); //adds the checkpoint to the list of checkpoints
         }
-        if (activeCheckpoint == null)
+        if (activeCheckpoint == null) //if there is no active checkpoint, the active checkpoint is set to the level start
         {
             activeCheckpoint = GameObject.Find("LevelStart").GetComponent<Checkpoint>();
         }
     }
 
-    public void SetActiveCheckpoint(Checkpoint checkpoint)
+    public void SetActiveCheckpoint(Checkpoint checkpoint) //sets the active checkpoint to the checkpoint passed in and deactivates all other checkpoints
     {
         foreach (Checkpoint cp in checkpoints)
         {
@@ -48,24 +51,23 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R)) //Temporarily used to test the respawn function
         {
             Respawn();
         }
     }
 
-    public void Respawn()
+    public void Respawn() //respawns the player at the active checkpoint
     {
-        GameObject player = GameObject.Find("Player");
+        GameObject player = GameObject.Find("Player"); //finds the player object and destroys it
         Destroy(player);
-            GameObject respawnPlayer = GameObject.Instantiate(
-                playerprefab,
-                new Vector3(activeCheckpoint.location.x, 1.08f, activeCheckpoint.location.z),
-                Quaternion.identity
-            );
-            respawnPlayer.name = "Player";
-            respawnPlayer.tag = "Player";
-            FindAnyObjectByType<CameraFollow>().Player = respawnPlayer;
+        GameObject respawnPlayer = GameObject.Instantiate(
+            playerprefab,
+            new Vector3(activeCheckpoint.location.x, PLAYERY, activeCheckpoint.location.z),
+            Quaternion.identity //respawns the player at the active checkpoint
+        );
+        respawnPlayer.name = "Player"; //sets the name and tag of the player object to "Player"
+        respawnPlayer.tag = "Player";
+        FindAnyObjectByType<CameraFollow>().Player = respawnPlayer; //finds the camera follow script and sets the player to the respawned player
     }
-    
 }
