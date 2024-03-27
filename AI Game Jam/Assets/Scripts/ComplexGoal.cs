9ad78862e5371c5c7205b0bae6bd86d0940a1c3e
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-03-28 10:00:39 +1300

    refactor: Moved all methods and checks for complex goal into a seperate class that inherits from the ItemGoal class

---
+++ AI Game Jam/Assets/Scripts/ComplexGoal.cs
@@ -0,0 +1,65 @@
+/*
+* Description: This is a child class of Item Goal and manages changed methods for complex type goals.
+* Author: Chase Bennett-Hill
+* Last Modified: 13 / 03 / 24
+* Last Modified By: Chase Bennett-Hill
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ComplexGoal : ItemGoal
+{
+    public List<GameObject> items;
+    public int itemsNeeded; //could become const
+
+    void Start()
+    {
+        isComplex = true;
+        items = new List<GameObject>();
+    }
+
+    public new bool CanUseObject(GameObject item)
+    {
+        Item i = item.GetComponent<Item>();
+
+        if (items.Count < itemsNeeded && i.Type == itemName && i.priority == items.Count + 1) //if the item is not already in the list and the item is the correct type
+        {
+            ShowHint(true, hintAction);
+
+            return true;
+        }
+        else
+        {
+            if (i.priority != items.Count + 1)
+            {
+                ShowHint(true, "Something is needed before \n this item can be placed here");
+            }
+            if (i.Type != itemName)
+            {
+                ShowHint(true, "This item cannot be used here");
+            }
+            if (items.Count >= itemsNeeded)
+            {
+                ShowHint(true, "No more items are needed here");
+            }
+
+            return false;
+        }
+    }
+
+    public new void UseObject(GameObject item = null)
+    {
+        Debug.Log("Item used in " + gameObject.name);
+        if (items.Count == itemsNeeded && item == null)
+        {
+            goalAction.Invoke();
+        }
+        else
+        {
+            items.Add(item);
+        }
+    }
+}
