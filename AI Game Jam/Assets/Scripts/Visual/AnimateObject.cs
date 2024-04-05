c55fbba2202c8c8efbd6dcb548f46644fb330e1f
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-04-05 18:24:11 +1300

    Fixed and refactored animation sscript

--- AI Game Jam/Assets/Scripts/Visual/AnimateObject.cs
+++ AI Game Jam/Assets/Scripts/Visual/AnimateObject.cs
@@ -1,62 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public enum AnimationType
+{
+    Move,
+    Rotate
+}
 
 public class AnimateObject : MonoBehaviour
 {
+    public bool isAnimating = false;
+    public AnimationType animationType;
+    public Vector3 target; //This is the target position or rotation relative to the object's current position or rotation
+    public float duration; //The time it takes to complete the animation
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartAnimation()
     {
-        //
+        isAnimating = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (isAnimating)
+        {
+            switch (animationType)
+            {
+                case AnimationType.Move:
+                    StartCoroutine(InterpolatePosition(transform.position, target, duration));
+                    break;
+                case AnimationType.Rotate:
+                    StartCoroutine(
+                        InterpolateRotation(
+                            transform.localRotation,
+                            Quaternion.Euler(target),
+                            duration
+                        )
+                    );
+                    break;
+                default:
+                    Debug.Log("Error: Invalid animation type was selected");
+                    break;
+            }
+        }
     }
 
-    public void MoveDown()
+    private IEnumerator InterpolatePosition(
+        Vector3 startPosition,
+        Vector3 targetPosition,
+        float duration
+    )
     {
+        float elapsedTime = 0;
 
-        if (transform.position.y > -14)
+        while (elapsedTime < duration)
         {
-            transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y - 0.1f,
-                transform.position.z
+            transform.position = Vector3.Lerp(
+                startPosition,
+                targetPosition,
+                elapsedTime / duration
             );
-            StartCoroutine(Delay(1));
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
 
+        transform.position = targetPosition;
+        print("exit");
+        isAnimating = false;
     }
-    // public void OpenDoor()
-    // {
 
-    //     if (transform.localRotation.y < 65)
-    //     {
-    //         print(transform.localRotation.y);
-    //             transform.localRotation = Quaternion.Euler(
-    //             0,
-    //              transform.localRotation.y+ 20f,
-    //             0
-    //         );
-    //         StartCoroutine(Delay(1));
-    //         print(transform.localRotation.y);
-    //     }
-    //     {
-    //         print("exit");
-    //     }
-
-    // }
-
-
-    private IEnumerator Delay(int seconds)
+    private IEnumerator InterpolateRotation(
+        Quaternion startRotation,
+        Quaternion targetRotation,
+        float duration
+    )
     {
-        yield return new WaitForSeconds(1);
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            transform.localRotation = Quaternion.Lerp(
+                startRotation,
+                targetRotation,
+                elapsedTime / duration
+            );
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localRotation = targetRotation;
+        print("exit");
+        isAnimating = false;
     }
 }
-
-
-
