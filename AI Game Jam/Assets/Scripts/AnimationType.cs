c26976d69a14afc1a836d279fcda5e196f2e76ae
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-04-08 11:38:50 +1200

    DOC: Added More comments to AnimateObject

---
+++ AI Game Jam/Assets/Scripts/AnimationType.cs
@@ -0,0 +1,13 @@
+/*
+* Description: This enum defines the type of animations that can be performed on objects
+* Author: Chase Bennett-Hill
+* Last Modified: 8 / 04 / 24
+* Last Modified By: Chase Bennett-Hill
+*/
+
+
+public enum AnimationType
+{
+    Move, //The object will move to a target position
+    Rotate //The object will rotate to a target rotation
+}
