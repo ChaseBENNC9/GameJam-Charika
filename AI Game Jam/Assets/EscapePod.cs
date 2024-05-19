d33646cb8088bcb1a89d75f10a3cf5c8b50b296d
ChaseBENNC9 <BENNC9@student.op.ac.nz>
2024-05-20 11:05:50 +1200

    add: prologue exit logic

---
+++ AI Game Jam/Assets/EscapePod.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EscapePod : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool inRange;
+
+    void Start()
+    {
+        inRange = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+    void Update()
+    {
+        if (inRange && Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerPrefs.SetInt("Level", 1);
+            PlayerPrefs.Save();
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
+        }
+    }
+}
