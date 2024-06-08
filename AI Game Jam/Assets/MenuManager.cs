3b29d3846f0fd8ac78b356c1c4680cd9b2f9045d
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-06-08 13:54:49 +1200

    fix: confirm modal now starts new game when confirmed

--- AI Game Jam/Assets/MenuManager.cs
+++ AI Game Jam/Assets/MenuManager.cs
@@ -6,60 +6,85 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     [Header("Main Menu")]
-    [SerializeField] private Button btnContinue;
-    [SerializeField] private Button btnLevelSelect;
+    [SerializeField]
+    private Button btnContinue;
+
+    [SerializeField]
+    private Button btnLevelSelect;
 
     [Header("Screens")]
-    [SerializeField] private GameObject titleScreenParent;
-    [SerializeField] private GameObject optionScreenParent;
-    [SerializeField] private GameObject levelSelectScreenParent;
+    [SerializeField]
+    private GameObject titleScreenParent;
+
+    [SerializeField]
+    private GameObject optionScreenParent;
+
+    [SerializeField]
+    private GameObject levelSelectScreenParent;
 
     [Header("Options Menu")]
-    [SerializeField] private Toggle tglFullscreen;
-    [SerializeField] private Button btnReset;
+    [SerializeField]
+    private Toggle tglFullscreen;
+
+    [SerializeField]
+    private Button btnReset;
+
     [Header("Modals")]
-    [SerializeField] private GameObject newGameModal;
+    [SerializeField]
+    private GameObject newGameModal;
 
     [Header("Level Select Menu")]
-    [SerializeField] private Button btnPrologue;
-    [SerializeField] private Button btnLevel1;
-    [SerializeField] private Button btnLevel2;
+    [SerializeField]
+    private Button btnPrologue;
 
+    [SerializeField]
+    private Button btnLevel1;
+
+    [SerializeField]
+    private Button btnLevel2;
 
     // Start is called before the first frame update
     void Start()
     {
-        btnContinue.interactable = GameSettings.Level >= 0; 
+        btnContinue.interactable = GameSettings.Level >= 0;
         btnReset.interactable = GameSettings.Level >= 0;
         btnLevelSelect.interactable = GameSettings.Level >= 0;
         btnLevel1.interactable = GameSettings.Level >= 1;
-        btnLevel2.interactable = false; ///Temp Force off
+        btnLevel2.interactable = false;
+        ///Temp Force off
         ShowTitle();
         Debug.Log("Level: " + GameSettings.Level);
     }
+
     public void QuitGame()
     {
-    
         Application.Quit();
     }
 
-    public void NewGame()
+    public void NewGame(bool force = false)
     {
-        if(GameSettings.Level >= 0)
+        if (force)
         {
-            newGameModal.SetActive(true);
+            GameSettings.Level = 0;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Prologue");
         }
         else
         {
-       
-        GameSettings.Level = 0;
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Prologue");
+            if (GameSettings.Level >= 0)
+            {
+                newGameModal.SetActive(true);
+            }
+            else
+            {
+                GameSettings.Level = 0;
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Prologue");
+            }
         }
     }
 
     public void LoadGame()
     {
-        switch(GameSettings.Level)
+        switch (GameSettings.Level)
         {
             case 0:
                 LoadLevel("Prologue");
@@ -83,19 +108,28 @@ public class MenuManager : MonoBehaviour
         titleScreenParent.SetActive(false);
         optionScreenParent.SetActive(true);
         levelSelectScreenParent.SetActive(false);
+        btnReset.interactable = GameSettings.Level >= 0;
     }
+
     public void ShowTitle()
     {
         titleScreenParent.SetActive(true);
         optionScreenParent.SetActive(false);
         levelSelectScreenParent.SetActive(false);
+        btnContinue.interactable = GameSettings.Level >= 0;
+        btnLevelSelect.interactable = GameSettings.Level >= 0;
     }
+
     public void ShowLevelSelect()
     {
         titleScreenParent.SetActive(false);
         optionScreenParent.SetActive(false);
         levelSelectScreenParent.SetActive(true);
+        btnLevel1.interactable = GameSettings.Level >= 1;
+        btnLevel2.interactable = false;
+        ///Temp Force off
     }
+
     public void ToggleFullscreen()
     {
         Screen.fullScreen = tglFullscreen.isOn;
