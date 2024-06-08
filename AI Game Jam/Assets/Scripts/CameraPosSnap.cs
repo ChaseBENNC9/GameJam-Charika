ba125400c347388cca5c480be495ad157f1b5044
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-06-08 15:18:32 +1200

    Added bedroom camera

--- AI Game Jam/Assets/Scripts/CameraPosSnap.cs
+++ AI Game Jam/Assets/Scripts/CameraPosSnap.cs
@@ -10,12 +10,16 @@ public class CameraPosSnap : MonoBehaviour
     // Second Floor
     // y = 17.47
     // z = -24.24
+    // Bedroom
+    // z = 2.85
 
     [SerializeField] private GameObject cam;
     private bool isIndoors;
     private bool hideWalls;
+    private bool inBedroom;
     [SerializeField] private List<GameObject> hideObjects = new List<GameObject>(); // walls that have children with mesh renderers
     [SerializeField] private List<GameObject> exceptionWalls = new List<GameObject>(); // walls that have a mesh renderer attached
+    [SerializeField] private List<GameObject> library = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +30,7 @@ public class CameraPosSnap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnTriggerEnter(Collider other)
@@ -36,19 +40,27 @@ public class CameraPosSnap : MonoBehaviour
         {
             isIndoors = true;
             cam.transform.position = new Vector3(transform.position.x, 7.09f, -24.24f);
-            //Debug.Log("Indoors");
         }
         else if (other.gameObject.tag == "FirstFloor" && isIndoors)
         {
             isIndoors = false;
             cam.transform.position = new Vector3(transform.position.x, 6.7f, -21.1f);
-            //Debug.Log("Outdoors");
         }
         
+        // Changes camera position when player is in the second floor hallway
         if (other.gameObject.tag == "SecondFloor")
         {
             cam.transform.position = new Vector3(transform.position.x, 17.47f, -24.24f);
-            Debug.Log("Second Floor");
+        }
+
+        if (other.gameObject.name == "Camera Bedroom" && !inBedroom)
+        {
+            inBedroom = true;
+            cam.transform.position = new Vector3(transform.position.x, 17.47f, 2.85f);
+            for (int i = 0; i < library.Count; i++)
+            {
+                library[i].SetActive(false);
+            }
         }
 
         
@@ -62,6 +74,7 @@ public class CameraPosSnap : MonoBehaviour
 
         }
 
+        // Hides walls when player is in the second floor hallway
         if (other.gameObject.tag == "SecondFloor")
         {
             for (int i = 0; i < hideObjects.Count; i++)
@@ -76,6 +89,7 @@ public class CameraPosSnap : MonoBehaviour
                 exceptionWalls[i].GetComponent<MeshRenderer>().enabled = false;
             }
         }
+
     }
 
     public void OnTriggerExit(Collider other)
@@ -92,6 +106,17 @@ public class CameraPosSnap : MonoBehaviour
             for (int i = 0; i < exceptionWalls.Count; i++)
             {
                 exceptionWalls[i].GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
+        
+        // Changes camera position when player is in the bedroom
+        else if (other.gameObject.name == "Camera Bedroom" && inBedroom)
+        {
+            inBedroom = false;
+            cam.transform.position = new Vector3(transform.position.x, 17.47f, -24.24f);
+            for (int i = 0; i < library.Count; i++)
+            {
+                library[i].SetActive(true);
             }
         }
     }
