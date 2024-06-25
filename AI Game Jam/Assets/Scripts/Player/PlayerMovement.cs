6048ce4684dcac86a07b3bf8275dc1db5081b91a
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-06-25 16:29:04 +1200

    add: player model working with no jump animation

--- AI Game Jam/Assets/Scripts/Player/PlayerMovement.cs
+++ AI Game Jam/Assets/Scripts/Player/PlayerMovement.cs
@@ -45,7 +45,6 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButton("Jump") && groundedPlayer) //If the player presses the jump button and is touching the ground
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * GRAVITYVALUE);
-            anim.SetTrigger("Jump");
         }
 
         playerVelocity.y += GRAVITYVALUE * Time.deltaTime;
